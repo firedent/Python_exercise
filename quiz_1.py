99eaea8eabb00d25721a00e9e5679d30b2b644f5
zscself <zscself@gmail.com>
2018-08-03 21:05:59 +1000

    提高代码简洁性和降低运行时间

--- quiz_1.py
+++ quiz_1.py
@@ -1,10 +1,8 @@
 # Written by *** and Eric Martin for COMP9021
 
 
-
 import sys
 from random import seed, randrange
-
 
 try:
     arg_for_seed = int(input('Enter an integer: '))
@@ -25,6 +23,14 @@ max_gap = -1
 first_and_last = set()
 
 # REPLACE THIS COMMENT WITH YOUR CODE
+
+distinct_digits = [0] * 9
+L_resolved = []
+d = dict()
+l_of_gap = []
+num_of_ele_in_dict = 0
+
+
 def resolve(e):
     if e == 0:
         return [0]
@@ -36,15 +42,15 @@ def resolve(e):
     l.reverse()
     return l
 
-L_resolved = list(map(resolve, L))
 
-#The sum of all digits in x
 sum_of_digits_in_x = sum(resolve(x))
 
-# smaller, greater or smaller
+for i in L:
+    num_of_ele = len(set(resolve(i)))
+    distinct_digits[num_of_ele] = distinct_digits[num_of_ele] + 1
+    L_resolved.append(resolve(i))
 
 for i in L_resolved:
-
     if i[0] > i[-1]:
         first_digit_greater_than_last += 1
     elif i[0] == i[-1]:
@@ -52,27 +58,15 @@ for i in L_resolved:
     else:
         last_digit_greater_than_first += 1
 
-# The number of members of L with X distinct digits
-distinct_digits = [0] * 9
-for i in L:
-    num_of_ele = len(set(resolve(i)))
-    distinct_digits[num_of_ele] = distinct_digits[num_of_ele] + 1
-
-
-l_of_gap = []
-for i in L_resolved:
     l_of_gap.append(abs(i[0] - i[-1]))
+
+    d[(i[0], i[-1])] = d.get((i[0], i[-1]), 0) + 1
+
 l_of_gap.sort()
 min_gap = l_of_gap[0]
 max_gap = l_of_gap[-1]
 
-
-d = dict()
-for i in L_resolved:
-    d[(i[0], i[-1])] = d.get((i[0], i[-1]), 0) + 1
-
 l_of_dict = sorted(d.items(), key=lambda a: a[1], reverse=True)
-num_of_ele_in_dict = 0
 for i in l_of_dict:
     if i[1] < num_of_ele_in_dict:
         break
@@ -89,7 +83,7 @@ print(f'There are {first_digit_greater_than_last}, {same_first_and_last_digits} 
       f'and {last_digit_greater_than_first} elements in L with a first digit that is\n'
       '  greater than the last digit, equal to the last digit,\n'
       '  and smaller than the last digit, respectively.'
-     )
+      )
 print()
 for i in range(1, 9):
     if distinct_digits[i]:
@@ -97,10 +91,10 @@ for i in range(1, 9):
 print()
 print('The minimal gap (in absolute value) between first and last digits\n'
       f'  of a member of L is {min_gap}.'
-     )
+      )
 print('The maximal gap (in absolute value) between first and last digits\n'
       f'  of a member of L is {max_gap}.')
 print()
 print('The number of pairs (f, l) such that f and l are the first and last digits\n'
       f'of members of L is maximal for (f, l) one of {sorted(first_and_last)}.'
-     )
+      )
