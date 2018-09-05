9d6ef70c006e86df1065f89931cb8a5284b52486
zscself <zscself@gmail.com>
2018-09-05 11:32:45 +1000

    Quiz5 没啥说的

--- playground.py
+++ playground.py
@@ -1,73 +1,8 @@
-# coding=utf-8
-# Randomly fills a grid of size 10 x 10 with 0s and 1s,
-# in an estimated proportion of 1/2 for each,
-# and computes the longest leftmost path that starts
-# from the top left corner -- a path consisting of
-# horizontally or vertically adjacent 1s --,
-# visiting every point on the path once only.
-#
-# Written by *** and Eric Martin for COMP9021
+def visit(L):
+    l.remove(0)
 
-
-import sys
-from random import seed, randrange
-
-from queue_adt import *
-
-
-def display_grid():
-    for i in range(len(grid)):
-        print('   ', ' '.join(str(grid[i][j]) for j in range(len(grid[0]))))
-
-
-def leftmost_longest_path_from_top_left_corner():
-    path = []
-    new_directions = {'': ['s', 'e'], 'n': ['e', 'n', 'w'], 'e': ['s', 'e', 'n'], 's': ['w', 's', 'e'],
-                      'w': ['n', 'w', 's']}
-    distance_dirt = {'n': (0, -1), 'e': (1, 0), 's': (0, 1), 'w': (-1, 0)}
-    if grid[0][0] != 0:
-        paths_quene = Queue()
-        paths_quene.enqueue(([(0, 0)], ['s', 'e']))
-        while not paths_quene.is_empty():
-            node = paths_quene.dequeue()
-            path = node[0]
-            destinaton = path[-1]
-            for direction in node[1]:
-                distance = distance_dirt.get(direction)
-
-                new_destinaton = (destinaton[0] + distance[1], destinaton[1] + distance[0])
-
-                directions = new_directions.get(direction)
-
-                if (new_destinaton[0] not in range(10)) | (new_destinaton[1] not in range(10)):
-                    continue
-                print(new_destinaton[0], new_destinaton[1])
-                if grid[new_destinaton[0]][new_destinaton[1]] == 0:
-                    continue
-                if new_destinaton in path:
-                    continue
-
-                new_path = path.copy()
-                new_path.append(new_destinaton)
-                new_node = (new_path, directions)
-                paths_quene.enqueue(new_node)
-
-    return path
-
-
-provided_input = input('Enter one integer: ')
-try:
-    for_seed = int(provided_input)
-except ValueError:
-    print('Incorrect input, giving up.')
-    sys.exit()
-seed(for_seed)
-grid = [[randrange(2) for _ in range(10)] for _ in range(10)]
-print('Here is the grid that has been generated:')
-display_grid()
-path = leftmost_longest_path_from_top_left_corner()
-if not path:
-    print('There is no path from the top left corner.')
-else:
-    print(f'The leftmost longest path from the top left corner is: {path}')
-
+a = [0,0,0]
+a = set(a)
+print(a)
+visit(a)
+print(a)
--- quiz_6.py
+++ quiz_6.py
@@ -7,6 +7,7 @@
 
 from random import seed, randrange
 import sys
+from queue import Queue
 
 
 dim = 10
@@ -19,26 +20,68 @@ def display_grid():
     print()
 
 
+def visit_point(start_point, grid):
+    i, j = start_point[0], start_point[1]
+    a, b = 1, 2
+    visited = []
+    visited_already_exist = 0
+    for _ in range(2):
+        a, b = b, a
+        for _ in range(2):
+            a = a*-1
+            for _ in range(2):
+                b = b*-1
+                v_i, v_j = i+a, j+b
+                if 0 <= v_i < len(grid) and 0 <= v_j < len(grid):
+                    if grid[v_i][v_j] == 1:
+                        visited.append((v_i, v_j))
+                    else:
+                        pass
+    return visited
+
+
 def explore_board():
-    pass
-    # Replace pass above with your code
+    chess = 0
+    chess_set = set()
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == 1:
+                chess += 1
+                chess_set.add((i, j))
+
+    print(f'chess:{chess}, they are {chess_set}')
+    print(grid)
+    num = 0
+    while len(chess_set) != 0:
+        q = set()
+        q.add(chess_set.pop())
+        while len(q) != 0:
+            going_visit = q.pop()
+            visited = visit_point(going_visit, chess_set)
+            for i in visited:
+                q.add(i)
+        num += 1
+    return num
 
 
 try:
-    for_seed, n = (int(i) for i in input('Enter two integers: ').split())
+    # for_seed, n = (int(i) for i in input('Enter two integers: ').split())
+    for_seed, n = 0, -20
     if not n:
         raise ValueError
 except ValueError:
     print('Incorrect input, giving up.')
     sys.exit()
 
-seed(for_seed)
-if n > 0:
-    grid = [[randrange(n) > 0 for _ in range(dim)] for _ in range(dim)]
-else:
-    grid = [[randrange(-n) == 0 for _ in range(dim)] for _ in range(dim)]    
-print('Here is the grid that has been generated:')
-display_grid()
+# seed(for_seed)
+# if n > 0:
+#     grid = [[randrange(n) > 0 for _ in range(dim)] for _ in range(dim)]
+# else:
+#     grid = [[randrange(-n) == 0 for _ in range(dim)] for _ in range(dim)]
+grid = [[0, 0, 0, 1], [0, 1, 0, 0], [0, 0, 0, 1], [0, 1, 0, 0]]
+print(grid)
+# print('Here is the grid that has been generated:')
+# display_grid()
 nb_of_knights = explore_board()
 if not nb_of_knights:
     print('No chess knight has explored this board.')
