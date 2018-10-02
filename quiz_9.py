d222628e6b39682d37a87cd6125846464e00eb12
zscself <zscself@gmail.com>
2018-10-03 01:40:35 +1000

    Quiz9 start

---
+++ quiz_9.py
@@ -0,0 +1,54 @@
+# Randomly fills a grid of size 7 x 7 with NE, SE, SW, NW,
+# meant to represent North-East, South-East, North-West, South-West,
+# respectively, and starting from the cell in the middle of the grid,
+# determines, for each of the 4 corners of the grid, the preferred path amongst
+# the shortest paths that reach that corner, if any. At a given cell, it is possible to move
+# according to any of the 3 directions indicated by the value of the cell;
+# e.g., from a cell storing NE, it is possible to move North-East, East, or North.
+# At any given point, one prefers to move diagonally, then horizontally,
+# and vertically as a last resort.
+#
+# Written by *** and Eric Martin for COMP9021
+
+
+import sys
+from random import seed, choice
+from queue_adt import *
+
+def display_grid():
+    for row in grid:
+        print('    ', *row)
+
+
+def preferred_paths_to_corners():
+    pass
+    # replace pass above with your code
+
+
+try:
+    seed_arg = int(input('Enter an integer: '))
+except ValueError:
+    print('Incorrect input, giving up.')
+    sys.exit()
+    
+seed(seed_arg)
+size = 3
+dim = 2 * size + 1
+grid = [[0] * dim for _ in range(dim)]
+directions = 'NE', 'SE', 'SW', 'NW'
+
+grid = [[choice(directions) for _ in range(dim)] for _ in range(dim)]
+print('Here is the grid that has been generated:')
+display_grid()
+
+corners = (0, 0), (dim - 1, 0), (dim - 1, dim - 1), (0, dim - 1)
+paths = preferred_paths_to_corners()
+if not paths:
+    print('There is no path to any corner')
+    sys.exit()
+for corner in corners:
+    if corner not in paths:
+        print(f'There is no path to {corner}')
+    else:
+        print(f'The preferred path to {corner} is:')
+        print('  ', paths[corner])
