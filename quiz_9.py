a07b9aded7554014871644d5448f5d1cb56e8e4c
zscself <zscself@gmail.com>
2018-10-04 00:55:37 +1000

    Quiz9 finished

--- quiz_9.py
+++ quiz_9.py
@@ -15,14 +15,103 @@ import sys
 from random import seed, choice
 from queue_adt import *
 
+
 def display_grid():
     for row in grid:
         print('    ', *row)
 
 
+def get_children(grid, point):
+    """
+    >>> grid = [['NW', 'SW', 'NE', 'NW', 'SE', 'NE', 'NE'],\
+                ['NE', 'SW', 'NW', 'SE', 'NW', 'NE', 'SE'],\
+                ['NE', 'SE', 'NW', 'SW', 'SE', 'NW', 'SE'],\
+                ['NE', 'SE', 'NW', 'SE', 'SE', 'NE', 'NE'],\
+                ['SE', 'SE', 'SE', 'SE', 'SW', 'SW', 'SE'],\
+                ['SE', 'SE', 'SE', 'NW', 'SW', 'NE', 'SW'],\
+                ['SW', 'SE', 'SE', 'SW', 'NE', 'SW', 'SE']]
+    >>> get_children(grid, (3, 3))
+    [(4, 4), (4, 3), (3, 4)]
+    >>> get_children(grid, (0, 0))
+    [(-1, -1), (-1, 0), (0, -1)]
+    >>> get_children(grid, (0, 6))
+    [(-1, 7), (-1, 6), (0, 7)]
+    >>> get_children(grid, (6, 0))
+    [(7, -1), (7, 0), (6, -1)]
+    >>> get_children(grid, (6, 6))
+    [(7, 7), (7, 6), (6, 7)]
+    """
+    operators = {
+        'N': lambda p: (p[0], p[1] - 1),
+        'S': lambda p: (p[0], p[1] + 1),
+        'W': lambda p: (p[0] - 1, p[1]),
+        'E': lambda p: (p[0] + 1, p[1]),
+    }
+    x, y = point[0], point[1]
+    direction = grid[y][x]
+    return [operators[direction[1]](operators[direction[0]](point)), operators[direction[1]](point), operators[direction[0]](point)]
+
+
+def shortest_path_in_dijkstra(grid, destination):
+    """
+    >>> grid = [['SW', 'SW', 'NE', 'NW', 'SE', 'NE', 'SE'],\
+                ['NE', 'SW', 'NW', 'SE', 'NW', 'NE', 'SE'],\
+                ['NE', 'SE', 'NW', 'SW', 'SE', 'NW', 'SE'],\
+                ['NE', 'SE', 'NW', 'SE', 'SE', 'NE', 'NE'],\
+                ['SE', 'SE', 'SE', 'SE', 'SW', 'SW', 'SE'],\
+                ['SE', 'SE', 'SE', 'NW', 'SW', 'NE', 'SW'],\
+                ['NW', 'SE', 'SE', 'SW', 'NE', 'SW', 'SW']]
+    >>> shortest_path_in_dijkstra(grid, (0, 0))
+    [(3, 3), (4, 3), (5, 3), (5, 2), (4, 1), (3, 1), (3, 2), (2, 2), (1, 1), (0, 1), (0, 0)]
+    >>> shortest_path_in_dijkstra(grid, (6, 0))
+    [(3, 3), (4, 3), (5, 3), (5, 2), (5, 1), (6, 0)]
+    >>> shortest_path_in_dijkstra(grid, (6, 6))
+    [(3, 3), (4, 3), (5, 4), (5, 5), (6, 5), (6, 6)]
+    >>> shortest_path_in_dijkstra(grid, (0, 6))
+    []
+    """
+    centre = (3, 3)
+    q = Queue()
+    visited_point = set()
+    pre_point = dict()
+    path = []
+
+    q.enqueue(centre)
+    visited_point.add(centre)
+    while not q.is_empty():
+        p = q.dequeue()
+        children = get_children(grid, p)
+        if destination in children:
+            pre_point[destination] = p
+            break
+        for i in children:
+            if 0 <= i[0] <= len(grid)-1 and 0 <= i[1] <= len(grid)-1:
+                if i not in visited_point and i not in corners:
+                    q.enqueue(i)
+                    pre_point[i] = p
+                    visited_point.add(i)
+
+    if pre_point.get(destination) is None:
+        return path
+
+    path.append(destination)
+    pre = destination
+    while True:
+        pre = pre_point.get(pre)
+        path.append(pre)
+        if pre == centre:
+            break
+
+    return path[::-1]
+
+
 def preferred_paths_to_corners():
-    pass
-    # replace pass above with your code
+    paths = dict()
+    for i in corners:
+        path = shortest_path_in_dijkstra(grid, i)
+        if len(path)>0:
+            paths[i] = path
+    return paths
 
 
 try:
@@ -30,7 +119,7 @@ try:
 except ValueError:
     print('Incorrect input, giving up.')
     sys.exit()
-    
+
 seed(seed_arg)
 size = 3
 dim = 2 * size + 1
